c2fd8972674020e30d65072c04ebc7eb6de01345
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-06-02 03:49:47 +0300

    Upload Project
    Finish Footer Section

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -11,7 +11,7 @@ export const CallToAction = () => {
         <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">Get instant access</h2>
         <p className="text-xl text-white/70 mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
-          <input type="email" placeholder="your@email.com" className="py-3 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] flex-1"></input>
+          <input type="email" placeholder="your@email.com" className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"></input>
           <button className="bg-white text-black h-12 rounded-lg px-5">Get access</button>
         </form>
       </div>
