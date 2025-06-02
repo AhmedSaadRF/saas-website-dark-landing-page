4fea9866d024b4632f44b591ffe18a3660417a29
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-02 18:02:18 +0300

    Upload Project

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -5,8 +5,8 @@ import EcoSystemIcon from "../assets/icons/ecosystem.svg";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 
 export const Feature = ({ title, description }: { title: string, description: string }) => {
-  const offsetX = useMotionValue(0);
-  const offsetY = useMotionValue(0);
+  const offsetX = useMotionValue(-100);
+  const offsetY = useMotionValue(-100);
   const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px, black, transparent)`;
   const border = useRef<HTMLDivElement>(null);
   useEffect(() => {
