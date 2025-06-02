e10f37b568be55982050932c1c56fe2ac728ada3
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-02 16:52:30 +0300

    Upload Project
    Finish LogoTicker Section Animation

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
@@ -5,6 +7,7 @@ import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -20,12 +23,15 @@ export const LogoTicker = () => {
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
         <h2 className="text-xl text-center text-white/70">Trusted by the world&apos;s most innovative teams</h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))]">
-          <div className="flex gap-16">
+        <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:z-10 before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <motion.div transition={{duration: 10, ease: "linear", repeat: Infinity }} initial={{ translateX: 0 }} animate={{ translateX: "-50%" }} className="flex gap-16 flex-none pr-16">
             {images.map(({ src, alt }, index) => (
               <Image key={index} src={src} alt={alt} className="flex-none h-8 w-auto" />
             ))}
-          </div>
+            {images.map(({ src, alt }, index) => (
+              <Image key={index} src={src} alt={alt} className="flex-none h-8 w-auto" />
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>
